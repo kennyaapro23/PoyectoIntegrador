68070ccfe1a0c6c36ff2cc2c6b6bba04dd847abe
kennyaapro@hotmail.com <kennyaapro@hotmail.com>
2022-07-12 22:50:49 -0500

    naa

--- SysVentasG2/src/main/java/pe/edu/upeu/modelo/VentaDetalleTO.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.modelo;
-
-import lombok.Data;
-
-@Data
-public class VentaDetalleTO {    
-    public String idVentDet, idVenta, idProducto;
-    public double descuento, pu, cantidad, subtotal;
-}
